e4f664b1745c41c873e9ca26b0a346190accf7af
alvinnardo <982791535@qq.com>
2024-11-10 12:54:16 +0800

    [review #540] single-element-in-a-sorted-array (binary search)

--- leetcode/540.cpp
+++ leetcode/540.cpp
@@ -1,6 +1,48 @@
 #include "../template/head.h"
 #include <bits/stdc++.h>
 
+// 2024-11-10
+int singleNonDuplicate_review(vector<int> nums) {
+    // 思路：二分
+    // 如果相等，判断下标是不是偶数
+    // 时间 O(logn)，空间 O(1)
+    int i = 0, j = nums.size() - 1;
+    int cnt = 0, pre = 0;
+    while (i <= j) {
+        int mid = i + ((j - i) >> 1);
+        pre = cnt = 0;
+        if (mid - 1 >= 0) {
+            if (nums[mid] == nums[mid - 1]) {
+                cnt++;
+                pre = 1;
+            }
+        }
+        if (mid + 1 < nums.size()) {
+            cnt += nums[mid] == nums[mid + 1];
+        }
+        if (cnt == 0) {
+            return nums[mid];
+        }
+
+        // 判断下标
+        if (pre) {
+            if ((mid - 1) & 1) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        } else {
+            if (mid & 1) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
 int singleNonDuplicate(vector<int> nums) {
     // 思路：二分
     // 因为是排好序的，则可以二分，每次在奇数范围中找
