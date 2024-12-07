0973ed3d1c8f05db84bc30e641400533418ca976
alvinnardo <982791535@qq.com>
2024-12-07 10:53:46 +0800

    [review #688] knight-probability-in-chessboard (dp)

--- leetcode/688.cpp
+++ leetcode/688.cpp
@@ -1,7 +1,38 @@
 #include "../template/head.h"
 #include <bits/stdc++.h>
 
+// review：空间优化
 double knightProbability(int n, int k, int row, int column) {
+    // 思路: DP
+    // 时间 O(n2k)，空间 O(n2)
+
+    vector<vector<int>> mov{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2},
+                            {1, -2},  {1, 2},  {2, -1},  {2, 1}};
+    vector<vector<double>> chess(n, vector<double>(n, 1));
+    for (int a = 0; a < k; a++) {
+        vector<vector<double>> tmp(n, vector<double>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                double t = 0;
+                int newi = 0, newj = 0;
+                for (const auto &m : mov) {
+                    newi = i + m[0];
+                    newj = j + m[1];
+                    if (newi >= 0 && newi < n && newj >= 0 && newj < n) {
+                        t += 0.125 * chess[newi][newj];
+                    }
+                }
+                tmp[i][j] = t;
+            }
+        }
+        chess = tmp;
+        // printVectorVector(chess);
+    }
+
+    return chess[row][column];
+}
+
+double knightProbability1(int n, int k, int row, int column) {
     // 思路：DP，每步是一个维度
     // 时间 O(n2k)，空间 O(n2k)
     vector<vector<vector<double>>> dp(
