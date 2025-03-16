9a628d98756ec1ae39625d14bd2644bb9e4e9a3f
alvinnardo <hiderygreen@gmail.com>
2025-03-17 00:26:23 +0800

    [code #1963] minimum-number-of-swaps-to-make-the-string-balanced (...)

---
+++ leetcode/1963.cpp
@@ -0,0 +1,19 @@
+#include "../template/head.h"
+#include <bits/stdc++.h>
+
+int minSwaps(string s) {
+    // 思路
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    // 1
+    cout << minSwaps("][][") << endl;
+    // 2
+    cout << minSwaps("]]][[[") << endl;
+    // 0
+    cout << minSwaps("[]") << endl;
+
+    return 0;
+}
