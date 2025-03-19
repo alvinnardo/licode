3ac2cc03651be2f6aa00fa2998bd0062dc9add4f
alvin <982791535@qq.com>
2025-03-19 10:53:47 +0800

    [review #2610] convert-an-array-into-a-2d-array-with-conditions (sort)

--- leetcode/2610.cpp
+++ leetcode/2610.cpp
@@ -1,7 +1,26 @@
 #include "../template/head.h"
 #include <bits/stdc++.h>
-
 vector<vector<int>> findMatrix(vector<int> nums) {
+    // 思路：排序
+    // 时间 O(nlogn)，空间 O(1)
+    // 排序后，相同的数字放在不同行
+    sort(nums.begin(), nums.end());
+    int level = 0;
+    vector<vector<int>> res;
+    for (int i = 0; i < nums.size(); i++) {
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            level = 0;
+        }
+        if (res.size() < level + 1) {
+            res.emplace_back();
+        }
+        res[level++].push_back(nums[i]);
+    }
+
+    return res;
+}
+
+vector<vector<int>> findMatrix1(vector<int> nums) {
     // 思路：哈希表
     // 时间 O(n)，空间 O(n)
     vector<int> stat(nums.size() + 1);
