46cdffc6721bf41777f800097ea2a562faeae213
alvin <982791535@qq.com>
2025-04-01 17:54:47 +0800

    [code #2140] ...

---
+++ leetcode/2140.cpp
@@ -0,0 +1,17 @@
+#include "../template/head.h"
+#include <bits/stdc++.h>
+
+long long mostPoints(vector<vector<int>> questions) {
+    // 思路
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    // 5
+    cout << mostPoints({{3, 2}, {4, 3}, {4, 4}, {2, 5}}) << endl;
+    // 7
+    cout << mostPoints({{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}) << endl;
+
+    return 0;
+}
