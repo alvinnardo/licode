2fec009d7a1baf38a9f49b91e2514fac7132a455
alvinnardo <hiderygreen@gmail.com>
2025-09-30 16:45:37 +0800

    [review #2221] find-triangular-sum-of-an-array (simulate)

---
+++ leetcode/2221.v.cpp
@@ -0,0 +1,28 @@
+#include "../template/head.h"
+#include <bits/stdc++.h>
+
+int triangularSum(vector<int> &&nums) {
+    // 思路：模拟
+    // 时间 O(n2)，空间 O(1)
+    int sz = nums.size();
+    if (sz == 1) {
+        return nums[0];
+    }
+
+    for (int i = 1; i < sz; i++) {
+        for (int j = sz - 1; j >= i; j--) {
+            nums[j] = (nums[j] + nums[j - 1]) % 10;
+        }
+    }
+
+    return nums.back();
+}
+
+int main(int argc, char *argv[]) {
+    // 8
+    cout << triangularSum({1, 2, 3, 4, 5}) << endl;
+    // 5
+    cout << triangularSum({5}) << endl;
+
+    return 0;
+}
